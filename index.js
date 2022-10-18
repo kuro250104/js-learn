95a34703f87625ad25c0f17037d9df723ecedfaa
Kuro <45374906+kuro250104@users.noreply.github.com>
2022-10-18 13:07:32 +0200

    UPDATE finish first step

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const forms= document.querySelector('#forms');
+const nom=document.querySelector('#nom')
+const prenom=document.querySelector('#prenom')
+
 forms.addEventListener('submit', function(e){
-    document.
     e.preventDefault();
+    addRow(document.getElementById('table'),nom, prenom);
+    nom.value = ""
+    prenom.value = ""
     console.log("je marche");
 });
+
+
+function addRow(elmt, nom, prenom)
+{
+    let tr = document.createElement('tr');
+    elmt.appendChild(tr); 
+    let tdNom = document.createElement('td');
+    tr.appendChild(tdNom);
+    let tdNomValue = document.createTextNode(nom.value);
+    tdNom.appendChild(tdNomValue);
+    let tdPrenom = document.createElement('td');
+    tr.appendChild(tdPrenom);
+    let tdPrenomValue = document.createTextNode(prenom.value);
+    tdPrenom.appendChild(tdPrenomValue);
+}
